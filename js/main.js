6a358d47b91ae6394d8ea3e025b71a417f4e6a34
Ketrin <geleczkak26@gmail.com>
2021-07-19 20:53:19 +0300

    Finish

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function between(a,b) {
+    let newArray = [];
+
+    for (let i = a; i <= b ; i++) {
+        newArray.push(i);
+    }
+    console.log(newArray);
+}
+
+between(1,4);
+between(-2, 2);
+between(20, 25);
